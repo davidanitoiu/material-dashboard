da5141b7db44deb8af5e62058c08959a5e7a64fd
Sarek <saregoth@yahoo.com>
2019-11-24 19:58:34 +0100

    Updated test coverage. Migrated App.js to rootDir.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-import App from '@app/App';
+import App from './App';
 import { Provider } from 'react-redux';
 import store from '@utils/Store';
 
