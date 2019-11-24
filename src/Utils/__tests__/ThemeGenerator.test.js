22e0e83b76f4eab1e78e0129e0b58d0985e9715f
Sarek <saregoth@yahoo.com>
2019-11-24 01:15:42 +0100

    Tests for ThemeGenerator

---
+++ src/Utils/__tests__/ThemeGenerator.test.js
@@ -0,0 +1,22 @@
+import { createTheme, material } from '@utils/ThemeGenerator';
+
+describe('ThemeGenerator', () => {
+    describe('Create Theme', () => {
+        it('When all variables are provided, a new theme is created #themeColors', () => {
+            const theme = createTheme({ primary: 'indigo', accent: 'yellow', type: 'light' });
+            expect(theme).toMatchSnapshot();
+        })
+        it('When variables are missing, an error occurs #themeColors', () => {
+            expect(() => createTheme()).toThrowErrorMatchingSnapshot();
+            expect(() => createTheme({})).toThrowErrorMatchingSnapshot();
+        })
+    })
+    describe('Generate Material Palette', () => {
+        it('When a color is passed, a palette is generated #themeColors', () => {
+            expect(material('indigo')).toMatchSnapshot();
+        })
+        it('When a color is passed, a palette is generated #themeColors', () => {
+            expect(material()).toThrowErrorMatchingSnapshot();
+        })
+    })
+})
