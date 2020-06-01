1b997102340e832bc9852d7359193e998a4a74e9
David Anitoiu <david.anitoiu@gmail.com>
2020-06-01 18:47:55 +0200

    Removed fortawesome. Install react-icons & config changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import '@utils/FontAwesomeWrapper';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Router from '@utils/Router';
 import { connect } from 'react-redux';
 import { createTheme } from '@utils/ThemeGenerator';
 
-function App(props) {
-  const theme = createTheme(props.themeColor);
+function App({themeColor}) {
+  const theme = createTheme(themeColor);
 
   return (
     <ThemeProvider theme={theme}>
