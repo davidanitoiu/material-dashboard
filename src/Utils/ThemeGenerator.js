3b034da37cbeb3b46bfe1e8c91bbe9e2ea9f088f
David Anitoiu <david.anitoiu@gmail.com>
2020-06-01 18:50:10 +0200

    Changed ThemeGenerator code

--- src/Utils/ThemeGenerator.js
+++ src/Utils/ThemeGenerator.js
@@ -1,20 +1,20 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import * as materialColors from '@material-ui/core/colors';
 
-export function createTheme({ primary, accent, type }) {
-  return responsiveFontSizes(createMuiTheme({
-    palette: {
-      type: type,
-      primary: materialColors[primary],
-      secondary: materialColors[accent],
-      action: {
-        selected: materialColors[primary][400],
-        hover: materialColors[primary][100],
-        disabled: '#BDBDBD',
+export const createTheme = ({ primary, accent, type }) =>
+  responsiveFontSizes(
+    createMuiTheme({
+      palette: {
+        type: type,
+        primary: materialColors[primary],
+        secondary: materialColors[accent],
+        action: {
+          selected: materialColors[primary][400],
+          hover: materialColors[primary][100],
+          disabled: '#BDBDBD',
+        }
       }
-    }
-  }));
-}
+    }));
 
 export const material = color => createMuiTheme({
   palette: {
