18522136e11ab540c2408716b8d394f727791383
David Anitoiu <david.anitoiu@gmail.com>
2020-06-02 17:16:54 +0200

    Updates

---
+++ precache-manifest.8a1f1eb97addfc2f70258674365aaca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8b851004197ec7c01239e55b18eabc",
+    "url": "/material-dashboard/index.html"
+  },
+  {
+    "revision": "de0bf8200463cc227d5b",
+    "url": "/material-dashboard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1e31c8fe581c4ca21ebe",
+    "url": "/material-dashboard/static/js/2.aa930469.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-dashboard/static/js/2.aa930469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0bf8200463cc227d5b",
+    "url": "/material-dashboard/static/js/main.71df1ec6.chunk.js"
+  },
+  {
+    "revision": "3943c0ec059aade9f942",
+    "url": "/material-dashboard/static/js/runtime-main.960eee90.js"
+  }
+]);
